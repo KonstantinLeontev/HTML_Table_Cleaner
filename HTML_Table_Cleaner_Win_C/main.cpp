a79d2f7ba651d14f6c3a878d06b5e4800e983ebb
Konstantin <KonstantinLeontev@users.noreply.github.com>
2018-03-30 21:30:20 +0300

    Add icon and menu resources

--- HTML_Table_Cleaner_Win_C/main.cpp
+++ HTML_Table_Cleaner_Win_C/main.cpp
@@ -1,4 +1,5 @@
 #include <windows.h>
+#include "resource.h"
 
 const char g_szClassName[] = "myWindowClass";
 
@@ -17,12 +18,19 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
-	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_HTC_ICON));
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-	wc.lpszMenuName = NULL;
+	wc.lpszMenuName = MAKEINTRESOURCE(IDM_HTC_MENU);
 	wc.lpszClassName = g_szClassName;
-	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hIconSm = (HICON)LoadImage(
+		GetModuleHandle(NULL),
+		MAKEINTRESOURCE(IDI_HTC_ICON),
+		IMAGE_ICON,
+		GetSystemMetrics(SM_CXSMICON),
+		GetSystemMetrics(SM_CYSMICON),
+		LR_DEFAULTCOLOR
+	);
 
 	if (!RegisterClassEx(&wc)) {
 		MessageBox(NULL, "Window Register failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
@@ -57,9 +65,25 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 // Window procedure definition.
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 	switch (msg) {
-	case WM_CLOSE: DestroyWindow(hwnd); break;
-	case WM_DESTROY: PostQuitMessage(0); break;
-	default: return DefWindowProc(hwnd, msg, wParam, lParam);
+	case WM_LBUTTONDOWN:
+		// Show the path of our programm.
+	{
+		char szFileName[MAX_PATH];
+		// Get handle to current programm.
+		HINSTANCE hInstance = GetModuleHandle(NULL);
+
+		GetModuleFileName(hInstance, szFileName, MAX_PATH);
+		MessageBox(hwnd, szFileName, "This program is: ", MB_OK | MB_ICONINFORMATION);
+	}
+		break;
+	case WM_CLOSE: 
+		DestroyWindow(hwnd);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hwnd, msg, wParam, lParam);
 	}
 	return 0;
 }
