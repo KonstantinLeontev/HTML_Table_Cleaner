1698eea8c33c28e361e084bde1d226f8ce9bcf3e
Konstantin <KonstantinLeontev@users.noreply.github.com>
2018-04-03 20:23:31 +0300

    Edit box was added, need to be corrected though. ExampleCode folder added to .gitignore.

--- HTML_Table_Cleaner_Win_C/main.cpp
+++ HTML_Table_Cleaner_Win_C/main.cpp
@@ -4,6 +4,8 @@
 // Menu symbols.
 #define ID_FILE_EXIT 9001
 #define ID_HELP_ABOUT 9002
+// Text editor symbol.
+#define IDC_MAIN_EDIT 101
 
 const char g_szClassName[] = "myWindowClass";
 
@@ -65,8 +67,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 // Window procedure definition.
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 	switch (msg) {
-		// Sent when an application requested that a window be created by calling CreateWindow().
+	// Sent when an application requested that a window be created by calling CreateWindow().
 	case WM_CREATE: {
+		// Start the main menu creating.
 		HMENU hMenu, hSubMenu;
 		HICON hIcon, hIconSm;
 
@@ -105,9 +108,90 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 		else {
 			MessageBox(hwnd, "Could not load small icon!", "Error", MB_OK | MB_ICONERROR);
 		}
+		// End the main menu creating.
+
+		// Start the main editor window.
+		HWND hwndEdit;
+		LOGFONT lfDefault;
+		HFONT hfDefault;
+
+		hwndEdit = CreateWindowEx(
+			WS_EX_CLIENTEDGE, // Extended window style - border with a sunken edge.
+			"EDIT", // Predefined class name - rectangular child window into wich the user can type text.
+			"", // The window name.
+			// Window styles.
+			WS_CHILD | WS_VISIBLE | WS_VSCROLL | WS_HSCROLL | ES_MULTILINE | ES_AUTOVSCROLL | ES_AUTOHSCROLL,
+			0, // Initial horizontal position.
+			0, // Initial vertical position.
+			100, // The width of the window in device units. 
+			100, // The height of the window in device units.
+			hwnd, // A handle to the parent.
+			(HMENU)IDC_MAIN_EDIT, // For a child window specifices the child-window identifiere.
+			GetModuleHandle(NULL), // A handle to the instance of the module to be associated with the window.
+			NULL // Pointer to a value to be passed through the CREATESTRUCT. NULL if no additional data is needed.
+		);
+		if (hwndEdit == NULL) {
+			MessageBox(hwnd, "Could not create edit box.", "Error", MB_OK | MB_ICONERROR);
+		}
+
+		// Get system font.
+		// This structure retrieves the metrics associated with nonclient area or nonminimized windows.
+		NONCLIENTMETRICS editBoxMetrics;
+		// Set size of this structure.
+		editBoxMetrics.cbSize = sizeof(NONCLIENTMETRICS);
+
+		// Retrieves metrics.
+		bool result = SystemParametersInfo(
+			SPI_GETNONCLIENTMETRICS, // Retrieves the metrics.
+			sizeof(NONCLIENTMETRICS), // Size of the metrics structure.
+			&editBoxMetrics, // Structure wich stores retrieving metrics.
+			0
+		);
+		if (result) {
+			// Get LOGFONT structure of default font for message windows.
+			lfDefault = editBoxMetrics.lfMessageFont;
+			// Create logic font based on LOGFONT structure.
+			hfDefault = CreateFontIndirect(&lfDefault);
+			// Pass HFONT handle with message.
+			SendMessage(hwndEdit, WM_SETFONT, (WPARAM)hfDefault, MAKELPARAM(FALSE, 0));
+		}
+		else {
+			MessageBox(hwnd, "Could not obtain the system metrics.", "Error", MB_OK | MB_ICONERROR);
+		}	
 	}
 		break;
-		// Sent when the user selects a command item from a menu.
+
+	// Sent to a window after it's size has changed.
+	case WM_SIZE: {
+		HWND hwndEdit{};
+		RECT rcClient{}; // Structure that defines the coordinate of the upper-left and lower-right corner.
+		BOOL result{};
+
+		// Retrieves the coordinate of a  main window's client area.
+		result = GetClientRect(hwnd, &rcClient);
+		if (!result) {
+			MessageBox(hwnd, "Couldn't get the client area size.", "Error", MB_OK | MB_ICONERROR);
+		}
+
+		// Retrieves a handle to a control in the specified dialog box.
+		hwndEdit = GetDlgItem(hwnd, IDC_MAIN_EDIT);
+
+		result = SetWindowPos(
+			hwndEdit, // Handle to the window.
+			NULL, // Handle to the window to 
+			0, // Position of the left side.
+			0, // Position of the top side.
+			rcClient.right - 200, // Width in pixels.
+			rcClient.bottom, // Height in pixels.
+			SWP_NOZORDER // The window sizing and position flag. This one retains the current Z order.
+		);
+		if (!result) {
+			MessageBox(hwnd, "Could not set editbox's position.\nSetWindowPos() was failed.", "Error", MB_OK | MB_ICONERROR);
+		}
+	}
+		break;
+
+	// Sent when the user selects a command item from a menu.
 	case WM_COMMAND: {
 		switch (LOWORD(wParam)) {
 		case ID_FILE_EXIT:
@@ -121,17 +205,21 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 			break;
 		}
 	}
-		// Case for learning purposes for a while.
+
+	// Case for learning purposes for a while.
 	case WM_LBUTTONDOWN:{
 		// Empty for now.
 	}
 		break;
+
 	case WM_CLOSE: 
 		DestroyWindow(hwnd);
 		break;
+
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
+
 	default:
 		return DefWindowProc(hwnd, msg, wParam, lParam);
 	}
