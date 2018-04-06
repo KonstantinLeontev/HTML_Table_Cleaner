edbda9e5b630cbdb1168fdeba74a9b9211bf30c8
Konstantin <KonstantinLeontev@users.noreply.github.com>
2018-04-06 16:08:03 +0300

    File New, Open, SaveAs menu items and dialogs were added

--- HTML_Table_Cleaner_Win_C/main.cpp
+++ HTML_Table_Cleaner_Win_C/main.cpp
@@ -1,9 +1,15 @@
+// The WinAPI code based on the winprog.org tutorials.
+//////////////////////////////////////////////////////
+
 #include <windows.h>
 #include "resource.h"
 
 // Menu symbols.
-#define ID_FILE_EXIT 9001
-#define ID_HELP_ABOUT 9002
+#define ID_FILE_NEW 9000
+#define ID_FILE_OPEN 9001
+#define ID_FILE_SAVEAS 9002
+#define ID_FILE_EXIT 9003
+#define ID_HELP_ABOUT 9004
 // Text editor symbol.
 #define IDC_MAIN_EDIT 101
 
@@ -17,6 +23,15 @@ BOOL CALLBACK AboutDlgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 // Function to read a text file.
 BOOL LoadTextFileToEdit(HWND hEdit, LPCTSTR pszFileName);
+
+// Save text from an edit box.
+BOOL SaveTextFileFromEdit(HWND hEdit, LPCTSTR pszFileName);
+
+// Open file dialog.
+void DoFileOpen(HWND hwnd);
+
+// Save file dialog.
+void DoFileSave(HWND hwnd);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
 	WNDCLASSEX wc;
@@ -80,6 +95,9 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 
 		hSubMenu = CreatePopupMenu();
 		// Append a new item to the specified menu bar, drop-down menu, submenu or shortcut menu.
+		AppendMenu(hSubMenu, MF_STRING, ID_FILE_NEW, "&New");
+		AppendMenu(hSubMenu, MF_STRING, ID_FILE_OPEN, "&Open");
+		AppendMenu(hSubMenu, MF_STRING, ID_FILE_SAVEAS, "&Save As");
 		AppendMenu(
 			hSubMenu, // A handle to the menu to be changed.
 			MF_STRING, // Controls the appearance and behavior of the menu item. 
@@ -200,6 +218,15 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 		case ID_FILE_EXIT:
 			PostMessage(hwnd, WM_CLOSE, 0, 0);
 			break;
+		case ID_FILE_NEW:
+			SetDlgItemText(hwnd, IDC_MAIN_EDIT, "");
+			break;
+		case ID_FILE_OPEN:
+			DoFileOpen(hwnd);
+			break;
+		case ID_FILE_SAVEAS:
+			DoFileSave(hwnd);
+			break;
 		case ID_HELP_ABOUT:
 			int ret = DialogBox(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_ABOUT), hwnd, AboutDlgProc);
 			if (ret == -1) {
@@ -264,25 +291,131 @@ BOOL LoadTextFileToEdit(HWND hEdit, LPCTSTR pszFileName) {
 	if (hFile != INVALID_HANDLE_VALUE) {
 		DWORD dwFileSize;
 
+		// Check the file size so we know how much memory we need to allocate. 
 		dwFileSize = GetFileSize(hFile, NULL);
 		if (dwFileSize != 0xFFFFFFFF) {
 			LPSTR pszFileText;
 
-			pszFileText = GlobalAlloc(GPTR, dwFileSize + 1);
+			// Allocates the  specific number of bytes from the heap.
+			pszFileText = (LPSTR)GlobalAlloc(GPTR, dwFileSize + 1);
 			if (pszFileText != NULL)
 			{
+				// Receives the number of bytes read when using a synchronous hFile parameter.
 				DWORD dwRead;
 
 				if (ReadFile(hFile, pszFileText, dwFileSize, &dwRead, NULL))
 				{
 					pszFileText[dwFileSize] = 0; // Add null terminator.
+					// Changes the text of the specified window.
 					if (SetWindowText(hEdit, pszFileText))
 						bSuccess = TRUE;
 				}
+				// Frees specified global memory object and invalidates its memory.
 				GlobalFree(pszFileText);
 			}
 		}
 		CloseHandle(hFile);
 	}
 	return bSuccess;
+}
+
+// Save text from an edit box.
+BOOL SaveTextFileFromEdit(HWND hEdit, LPCTSTR pszFileName) {
+	HANDLE hFile;
+	BOOL bSuccess = FALSE;
+
+	hFile = CreateFile(
+		pszFileName, // The name of the file to be create.
+		GENERIC_WRITE, // The requested access to the file.
+		0, // The sharing mode. If it's zero and CF func succeeds, the file cannot be shared until the handle is closed.
+		NULL, // A pointer to security attribute.
+		CREATE_ALWAYS, // If specified file exists and is writable, the function overwrites the file.
+		FILE_ATTRIBUTE_NORMAL, // The most common default value for files - attributes and flags.
+		NULL // A handle to a template file.
+	);
+
+	if (hFile != INVALID_HANDLE_VALUE) {
+		DWORD dwTextLength;
+
+		// Retrieves the length, in characters, of the window's title bar text (if it has any).
+		// If the window is a control, retrieves the length of the text with control.
+		dwTextLength = GetWindowTextLength(hEdit);
+		// No need to bother if there's no text.
+		if (dwTextLength > 0) {
+			LPSTR pszText;
+			DWORD dwBufferSize = dwTextLength + 1;
+
+			// Allocate the specified number of bytes from the heap.
+			pszText = (LPSTR)GlobalAlloc(GPTR, dwBufferSize);
+			if (pszText != NULL) {
+				// Copies the text of the window's title bar (if it has one) or the text of the control.
+				if (GetWindowText(hEdit, pszText, dwBufferSize)) {
+					DWORD dwWritten;
+
+					if (WriteFile(
+						hFile, // Handle to the file.
+						pszText, // Pointer to the buffer containing the data to be written.
+						dwTextLength, // The number of bytes to be written.
+						&dwWritten, // A pointer to the variable that receives the number of bytes written.
+						NULL // A pointer to an OVERLAPPED structure.
+					)) {
+						// Change return variable if WriteFile() was succeed.
+						bSuccess = TRUE;
+					}
+				}
+			}
+		}
+	}
+	return bSuccess;
+}
+
+// Open file dialogue.
+void DoFileOpen(HWND hwnd) {
+	// Try some time later - starting with Windows Vista,
+	// the Common Item Dialog supersedes the older Common File Dialog.
+	// OPENFILENAME containes information that the GetOpenFileName and GetSaveFileName
+	// use to initialize dialog box.
+	OPENFILENAME ofn;
+	char szFileName[MAX_PATH] = "";
+
+	// Fills a block of memory with zeroes.
+	// This way we don't need to explicitely set each member that we don't use.
+	ZeroMemory(&ofn, sizeof(ofn));
+
+	// Fill ofn structure.
+	ofn.lStructSize = sizeof(ofn);
+	ofn.hwndOwner = hwnd;
+	ofn.lpstrFilter = "Text Files (*.txt)\0*.txt\0All Files (*.*)\0*.*\0";
+	ofn.lpstrFile = szFileName;
+	ofn.nMaxFile = MAX_PATH; // The size in characters of the buffer pointed to by lpstrFile.
+	ofn.Flags = OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_HIDEREADONLY;
+	ofn.lpstrDefExt = "txt"; // The default extension.
+
+	if (GetOpenFileName(&ofn)) {
+		// Retrieves a handle to a control in the specified dialog box.
+		HWND hEdit = GetDlgItem(hwnd, IDC_MAIN_EDIT);
+		LoadTextFileToEdit(hEdit, szFileName);
+	}
+}
+
+// Save file dialog.
+void DoFileSave(HWND hwnd) {
+	OPENFILENAME ofn;
+	char szFileName[MAX_PATH] = "";
+
+	ZeroMemory(&ofn, sizeof(ofn));
+
+	ofn.lStructSize = sizeof(ofn);
+	ofn.hwndOwner = hwnd;
+	ofn.lpstrFilter = "Text Files (*.txt)\0*.txt\0All Files (*.*)\0*.*\0";
+	ofn.lpstrFile = szFileName;
+	ofn.nMaxFile = MAX_PATH;
+	ofn.lpstrDefExt = "txt";
+	ofn.Flags = OFN_EXPLORER | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT;
+	ofn.lpstrDefExt = "txt";
+
+	if (GetSaveFileName(&ofn)) {
+		HWND hEdit = GetDlgItem(hwnd, IDC_MAIN_EDIT);
+		SaveTextFileFromEdit(hEdit, szFileName);
+	}
 }
